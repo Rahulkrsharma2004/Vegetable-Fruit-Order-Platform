4f82fd367cb883235e77a65b0b72329749bc0222
Rahulkrsharma2004 <kumarrahulbasdiha@gmail.com>
2024-11-29 16:46:20 +0530

    testing order

--- backend-app/controllers/orderController.js
+++ backend-app/controllers/orderController.js
@@ -1,24 +1,18 @@
 const Order = require('../models/order');
-
-
 const Product = require('../models/product');
+
 exports.placeOrder = async (req, res) => {
   const { productId, quantity, buyerName, contactInfo, deliveryAddress } = req.body;
   try {
-    // Fetch the product details to check the available stock
     const product = await Product.findById(productId);
     if (!product) {
       return res.status(404).json({ message: 'Product not found' });
     }
-    // Check if the product has enough stock
     if (product.stock < quantity) {
       return res.status(400).json({ message: `Only ${product.stock} items available` });
     }
-    // Deduct the ordered quantity from the product stock
     product.stock -= quantity;
-    // Save the updated product stock
     await product.save();
-    // Create a new order with the provided details
     const order = new Order({
       product: productId,
       quantity,
@@ -26,32 +20,12 @@ exports.placeOrder = async (req, res) => {
       contactInfo,
       deliveryAddress
     });
-    // Save the new order
     const savedOrder = await order.save();
     res.status(201).json({ message: 'Order placed successfully', order: savedOrder });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 };
-
-
-
-
-
-
-
-
-
-
-// exports.placeOrder = async (req, res) => {
-//   const order = new Order(req.body);
-//   try {
-//     const savedOrder = await order.save();
-//     res.status(201).json(savedOrder);
-//   } catch (error) {
-//     res.status(400).json({ message: error.message });
-//   }
-// };
 
 
 exports.getOrders = async (req, res) => {
@@ -62,6 +36,7 @@ exports.getOrders = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
 
 exports.updateOrderStatus = async (req, res) => {
   try {
